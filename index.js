01432763b1042498356ea0670e41ea70241855fd
Ivan Kudinov <ikudinov@ccsteam.ru>
2022-12-02 12:55:30 +0300

    Add cookie

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get(["/", "/:name"], (req, res) => {
-    greeting = "<h1>Hello From Node on Fly!</h1>";
-    res.send(greeting);
+    const html = `
+        <h1>Cookie test</h1>
+        <br/>
+        Cookies: ${req.headers.cookie}
+        <br/>
+        <button style="border:1px #eee solid; padding: 10px; margin: 10px 0" onclick="location.reload()">Reload</button>
+    `;
+    res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none', secure: true }).send(html);
 });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
