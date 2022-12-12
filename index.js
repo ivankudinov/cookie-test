2db3b0ba59e79bfe123108b520f4a40880d814ac
Ivan Kudinov <ikudinov@ccsteam.ru>
2022-12-12 12:18:08 +0300

    Force ksip endpoint

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.get("/ksip", (req, res) => {
     });
 });
 
-app.get(["/", "/:name"], (req, res) => {
-    const html = `
-        <h1>Cookie test</h1>
-        <br/>
-        <div style="width: 300px; word-break: break-all">Cookies: ${req.headers.cookie}</div>
-        <br/>
-        <button style="border:1px #eee solid; padding: 10px; margin: 10px 0" onclick="location.reload()">Reload</button>
-    `;
-    // res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none', secure: true }).send(html); // secure cookie
-    res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none' }).send(html);
-});
+// app.get(["/", "/:name"], (req, res) => {
+//     const html = `
+//         <h1>Cookie test</h1>
+//         <br/>
+//         <div style="width: 300px; word-break: break-all">Cookies: ${req.headers.cookie}</div>
+//         <br/>
+//         <button style="border:1px #eee solid; padding: 10px; margin: 10px 0" onclick="location.reload()">Reload</button>
+//     `;
+//     // res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none', secure: true }).send(html); // secure cookie
+//     res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none' }).send(html);
+// });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
