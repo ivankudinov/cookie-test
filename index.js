3f46e2422882dc4da7dde6f7d12268e11b10877b
Ivan Kudinov <ikudinov@ccsteam.ru>
2022-12-12 11:18:07 +0300

    Add ksip endpoint

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/ksip", (req, res) => {
+    res.json({
+        status: "ok",
+        cts_url: "https://cts3dev.ccsteam.ru"
+    });
+});
+
 app.get(["/", "/:name"], (req, res) => {
     const html = `
         <h1>Cookie test</h1>
@@ -12,7 +19,7 @@ app.get(["/", "/:name"], (req, res) => {
         <button style="border:1px #eee solid; padding: 10px; margin: 10px 0" onclick="location.reload()">Reload</button>
     `;
     // res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none', secure: true }).send(html); // secure cookie
-    res.cookie("testcookie", "cookieval1", { httpOnly: true }).send(html);
+    res.cookie("testcookie", "cookieval1", { httpOnly: true, sameSite: 'none' }).send(html);
 });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
